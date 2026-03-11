123ddd42029c1c978cb86395f84b9c78af728809
Ananya <aa7600@srmist.edu.in>
2026-03-12 01:22:33 +0530

    Fix UC2: correct command-line argument implementation

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,19 +1,10 @@
-/**
- * HelloApp.java
- * UC1: Display "Hello World"
- * UC2: Display "Hello" with command-line argument
- */
-
 public class HelloApp {
 
     public static void main(String[] args) {
 
-        if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        } else {
-            System.out.println("Hello, World!");
-        }
+        String name = args[0];
+        System.out.println("Hello, " + name + "!");
 
     }
+
 }
